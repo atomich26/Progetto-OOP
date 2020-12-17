a1b1f85a8f5c5b2bcd77615dfefc11f066ab1cb1
michdev7 <michele.bev@outlook.com>
2020-12-17 13:56:57 +0100

    Aggiunto FilterService

---
+++ restwebservice/src/main/java/it/michdev/restwebservice/service/FilterService.java
@@ -0,0 +1,30 @@
+package it.michdev.restwebservice.service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+import it.michdev.restwebservice.model.LiveQuote;
+import it.michdev.restwebservice.utils.filter.CurrencyFilter;
+
+public class FilterService {
+
+    public static ArrayList<LiveQuote> filterLiveQuotes(CurrencyFilter currencyFilter, ArrayList<LiveQuote> dataToFilter) {
+        List<LiveQuote> filteredList = new ArrayList<>();
+        filteredList.addAll(dataToFilter);
+        filteredList = filteredList
+                .stream().filter(o -> currencyFilter.checkCondition(o.getBaseCurrency(), o.getQuoteCurrency())).collect(Collectors.toList());
+        return (ArrayList<LiveQuote>) filteredList;
+    }
+
+    public static String buildCurrenciesQuery(CurrencyFilter currencyFilter) {
+        ArrayList<String> allCurrencies = new ArrayList<>();
+
+        for (String currencyParam : currencyFilter.getQuoteCurrenciesQuery()) {
+            if (currencyParam != currencyFilter.getParam())
+                allCurrencies.add(currencyFilter.getParam() + currencyParam);
+        }
+        return allCurrencies.stream().collect(Collectors.joining(","));
+    }
+}
