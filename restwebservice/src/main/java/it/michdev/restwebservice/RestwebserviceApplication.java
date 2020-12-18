1e107163004a26512aedb13ce68a38f5244b68e5
michdev7 <michele.bev@outlook.com>
2020-12-18 02:03:15 +0100

    Aggiornata Javadoc

--- restwebservice/src/main/java/it/michdev/restwebservice/RestwebserviceApplication.java
+++ restwebservice/src/main/java/it/michdev/restwebservice/RestwebserviceApplication.java
@@ -7,8 +7,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
  * Classe principale dell'applicazione Java. Contiene al suo interno il metodo
  * main per l'avvio del software.
  * 
- * @version 0.2.1
- * @since 0.1.0
+ * @version 0.8.0
  * @author Michele Bevilacqua
  */
 @SpringBootApplication
@@ -19,7 +18,6 @@ public class RestWebServiceApplication {
 	 * Questo metodo è il punto di partenza dell'esecuzione del software.
 	 * 
 	 * @param args argomenti scritti a riga di comando.
-	 * @since 0.1.0
 	 */
 	public static void main(String[] args) {
 		SpringApplication.run(RestWebServiceApplication.class, args);
