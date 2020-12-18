9242a21d5ae7e8ecfc4d576dc5c7cf2a52077ba0
michdev7 <michele.bev@outlook.com>
2020-12-18 19:20:58 +0100

    Aggiornata Javadoc

--- restwebservice/src/main/java/it/michdev/restwebservice/model/dataseries/TimeSeries.java
+++ restwebservice/src/main/java/it/michdev/restwebservice/model/dataseries/TimeSeries.java
@@ -1,7 +1,6 @@
 package it.michdev.restwebservice.model.dataseries;
 
 import java.util.ArrayList;
-
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
@@ -10,44 +9,78 @@ import it.michdev.restwebservice.utils.parser.DateParser;
 import it.michdev.restwebservice.utils.time.Period;
 
 /**
- * Ja
+ * La classe <code>TimeSeries</code> rappresenta una serie storica di valori di
+ * quotazioni associato ad un determinato periodo di tempo
+ * 
+ * @version 1.0.0
+ * @author Michele Bevilacqua
  */
-@JsonPropertyOrder({"code", "name","start_date", "end_date", "time_series"})
+@JsonPropertyOrder({ "code", "name", "start_date", "end_date", "time_series" })
 public class TimeSeries extends DataSeries<DataPoint> {
-    
+
     private Period timeSeriesPeriod;
     private ArrayList<DataPoint> dataPointList;
-    
+
+    /**
+     * Costruttore per la classe <code>TimeSeries</code>.
+     * 
+     * @param timeSeriesPeriod perido associato alla serie storica di dati.
+     */
     public TimeSeries(Period timeSeriesPeriod) {
         this.timeSeriesPeriod = timeSeriesPeriod;
     }
 
+    /**
+     * Costruttore per la classe <code>TiemSeries</code>
+     * 
+     * @param timeSeriesPeriod periodo associato alla serie storica di dati
+     * @param baseCurrencyCode codice della valuta associata alla serie storica.
+     */
     public TimeSeries(Period timeSeriesPeriod, String baseCurrencyCode) {
         super(baseCurrencyCode);
         this.timeSeriesPeriod = timeSeriesPeriod;
     }
-    
+
+    /**
+     * Restituisce la data iniziale del periodo associato alla serie storica
+     * @return stringa della data
+     */
     @JsonProperty("start_date")
     public String getStartDate() {
         return DateParser.getDateAsString(timeSeriesPeriod.getStartDate(), DateParser.YYYYMMDD);
     }
 
+     /**
+     * Restituisce la data finale del periodo associato alla serie storica.
+     * @return stringa della data
+     */
     @JsonProperty("end_date")
     public String getEndDate() {
         return DateParser.getDateAsString(timeSeriesPeriod.getEndDate(), DateParser.YYYYMMDD);
     }
 
+     /**
+     * Restituisce il periodo associato alla serie storica
+     * @return <code>Period</code> del periodo associato alla serie storica.
+     */
     @JsonIgnore
     public Period getPeriod() {
         return this.timeSeriesPeriod;
     }
 
+    /**
+     * Restituisce il dataset associato alla serie storica.
+     */
     @Override
     @JsonProperty("time_series")
     public ArrayList<DataPoint> getDataSeries() {
         return this.dataPointList;
     }
 
+    /**
+     * Imposta il dataset associato alla serie storica.
+     * @param dataList dataset
+     */
     @Override
     public void setDataSeries(ArrayList<DataPoint> dataList) {
         this.dataPointList = dataList;
