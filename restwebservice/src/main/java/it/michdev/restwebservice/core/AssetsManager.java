375a7ec29ee27caff8860ce70355b5fe784577ff
michdev7 <michele.bev@outlook.com>
2020-12-18 01:19:42 +0100

    Aggiunta javadoc per le classi del package core

--- restwebservice/src/main/java/it/michdev/restwebservice/core/AssetsManager.java
+++ restwebservice/src/main/java/it/michdev/restwebservice/core/AssetsManager.java
@@ -8,13 +8,13 @@ import it.michdev.restwebservice.model.CurrenciesList;
 import it.michdev.restwebservice.utils.parser.JsonParser;
 
 /**
- * <b>ApplicationAssets</b> costituisce un componente gestito per l'applicazione
- * Spring. Questa classe s'interfaccia con i dati contenuti nei file di risorse, quali:
- * <i>currencies.json</i>, <i>config.json</i>, <i>metadata.json</i>.
+ * <b>AssetsManager</b> costituisce un componente gestito per l'applicazione
+ * Spring. Questa classe s'interfaccia con i dati contenuti nei file di risorse,
+ * quali: <i>currencies.json</i>, <i>config.json</i>, <i>metadata.json</i>.
  * 
- * @version 0.2.5
- * @since 0.2.5
+ * @version 0.8.0
  * @author Michele Bevilacqua
+ * @see it.michdev.restwebservice.core.TaskManager
  */
 @Component
 public final class AssetsManager {
@@ -22,25 +22,44 @@ public final class AssetsManager {
     private static String accessKey;
     private static JsonNode metadata;
     private static CurrenciesList currenciesList;
-               
+
+    /**
+     * Inizializza i dati contenuti nei file della cartella Resources e li salva in
+     * memoria per un uso successivo da parte di altre classi.
+     */
     public static void initAssets() {
         try {
             accessKey = JsonParser.readNode(ResourceUtils.getFile("classpath:config.json")).get("access_key").asText();
             metadata = JsonParser.readNode(ResourceUtils.getFile("classpath:metadata.json"));
-            currenciesList = JsonParser.readNode(ResourceUtils.getFile("classpath:currencies.json"), CurrenciesList.class);
-        } catch (FileNotFoundException e) {        
+            currenciesList = JsonParser.deserialize(ResourceUtils.getFile("classpath:currencies.json"),
+                    CurrenciesList.class);
+        } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
     }
 
+    /**
+     * Restituisce l'access key salvato in config.json utile per effettuare le
+     * chiamate HTTP al servizio fxmarketapi.com
+     * 
+     * @return stringa dell'access key salvato.
+     */
     public static String getAccessKey() {
         return accessKey;
     }
 
+    /**
+     * Ottiene il contenuto presente nel file metadata.json
+     * @return oggetto <code>JsonNode</code>  
+     */
     public static JsonNode getMetadata() {
         return metadata;
     }
 
+    /**
+     * Ottiene la lista delle valute disponibili contenute nel file currencies.json
+     * @return oggetto <code>CurrenciesList</code>.
+     */
     public static CurrenciesList getCurrenciesList() {
         return currenciesList;
     }
--- restwebservice/src/main/java/it/michdev/restwebservice/core/TaskManager.java
+++ restwebservice/src/main/java/it/michdev/restwebservice/core/TaskManager.java
@@ -5,9 +5,24 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import it.michdev.restwebservice.service.DataService;
 
+/**
+ * La classe <b>TaskManager</b> rappresenta un componente gestito per
+ * l'applicazione Spring. Contiene tutti i metodi, la cui chiamata è programmata
+ * per mezzo di annotazioni Spring <code>PostContruct</code> o
+ * <code>Scheduled</code>.
+ * 
+ * @version 0.8.0
+ * @author Michele Bevilacqua
+ * @see it.michdev.restwebservice.core.AssetsManager
+ */
 @Component
 public final class TaskManager {
-    
+
+    /**
+     * Questo metodo viene eseguito all'avvio dell'applicazione Spring. Richiama
+     * metodi i quali necessitano di essere eseguite all'avvio, in particolare
+     * quelle per la creazione di un dataset.
+     */
     @PostConstruct
     public static void doLaunchTask() {
         AssetsManager.initAssets();
@@ -15,6 +30,11 @@ public final class TaskManager {
         System.out.println("$--> Dataset created");
     }
 
+    /**
+     * Questo metodo viene eseguito ogni 10 minuti. Richiama il metodo
+     * <code>updateDataSet</code> di <code>DataService</code> per aggiornare il
+     * dataset.
+     */
     @Scheduled(initialDelay = 600000, fixedRate = 600000)
     public static void doUpdateDataTask() {
         DataService.updateDataSet();
