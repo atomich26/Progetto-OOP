a4a4ae3c0ac8d7ed4288c7160efb5ffc467cd868
michdev7 <michele.bev@outlook.com>
2020-12-18 13:27:20 +0100

    Aggiunta eccezione InvalidStatsFieldException

---
+++ restwebservice/src/main/java/it/michdev/restwebservice/exception/InvalidStatsFieldException.java
@@ -0,0 +1,26 @@
+package it.michdev.restwebservice.exception;
+
+/**
+ * L'eccezione <b>InvalidStatsFieldException</b> estende <b>Exception</b>. Essa
+ * viene generata quando il periodo di tempo stabilito nel <code>body</code>
+ * della richiesta <code>POST</code> non è corretto. L'eccezione è gestita dalla
+ * classe <b>GlobalExceptionHandler</b>.
+ * 
+ * @version 0.8.0
+ * @author Michele Bevilacqua
+ * @see it.michdev.restwebservice.component.GlobalExceptionHandler
+ */
+public class InvalidStatsFieldException extends Exception {
+
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * Costruttore per la classe <b>InvalidStatsFieldException</b>
+     * 
+     * @param message descrizione dell'eccezione generata.
+     */
+    public InvalidStatsFieldException(String message) {
+        super(message);
+    }
+}
+
