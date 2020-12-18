1abb2a1aeb39bfa3eb9498a5325c9390a2162e36
michdev7 <michele.bev@outlook.com>
2020-12-18 01:07:47 +0100

    Aggiunta Javadoc per WebClient

--- restwebservice/src/main/java/it/michdev/restwebservice/webclient/FxMarketClient.java
+++ restwebservice/src/main/java/it/michdev/restwebservice/webclient/FxMarketClient.java
@@ -8,24 +8,49 @@ import it.michdev.restwebservice.core.AssetsManager;
 import it.michdev.restwebservice.utils.filter.PeriodFilter;
 import it.michdev.restwebservice.utils.parser.DateParser;
 
+/**
+ * La classe <code>FxMarketClient</code> estende <code>WebClient</code>.
+ * Rappresenta un HTTP REST client al servizio offerto da fxmarketsapi.com ed
+ * implementa i metodi per ottenere i dati necessari.
+ * 
+ * @version 0.8.0
+ * @author Michele Bevilacqua
+ * @see it.michdev.restwebservice.webclient.WebClient
+ */
 public class FxMarketClient extends WebClient {
 
     private URI requestUri;
     private String currenciesPairsQuery;
     private String accessKey;
 
+    /**
+     * Costruttore per la classe <code>FxMarketClient</code>.
+     */
     public FxMarketClient() {
         super("https://fxmarketapi.com");
         this.accessKey = AssetsManager.getAccessKey();
         this.currenciesPairsQuery = AssetsManager.getCurrenciesList().getCurrenciesPairsQuery();
     }
 
+    /**
+     * Effettua una chiamata per scaricare i dati sulle valute aggiornati in tempo
+     * reale.
+     * 
+     * @return oggetto <code>HttpResponse</code> contenente i dati ottenuti.
+     */
     public HttpResponse<String> requestData() {
         requestUri = UriComponentsBuilder.fromUriString(endpoint).path("apilive").queryParam("api_key", accessKey)
                 .queryParam("currency", currenciesPairsQuery).build().toUri();
         return downloadData(requestUri);
     }
 
+    /**
+     * Effettua una chiamata per scaricare i dati sulle valute in un determinato
+     * istante
+     * 
+     * @param date <code>Calendar</code> dell'istante di cui si vogliono i dati.
+     * @return oggetto <code>HttpResponse</code> contenente i dati ottenuti.
+     */
     public HttpResponse<String> requestData(Calendar date) {
         requestUri = UriComponentsBuilder.fromUriString(endpoint).path("apihistorical").queryParam("api_key", accessKey)
                 .queryParam("date", DateParser.getDateAsString(date, DateParser.YYYYMMDDHHmm))
@@ -33,11 +58,21 @@ public class FxMarketClient extends WebClient {
         return downloadData(requestUri);
     }
 
+    /**
+     * Effettua una chiamata per scaricare i dati sulle valute in un determinato
+     * intervallo di tempo.
+     * 
+     * @param date <code>Calendar</code> intervallo di tempo di cui si vogliono i
+     *             dati.
+     * @return oggetto <code>HttpResponse</code> contenente i dati ottenuti.
+     */
     public HttpResponse<String> requestData(PeriodFilter periodFilter, String quoteCurrenciesQuery) {
         requestUri = UriComponentsBuilder.fromUriString(endpoint).path("apitimeseries").queryParam("api_key", accessKey)
                 .queryParam("currency", quoteCurrenciesQuery)
-                .queryParam("start_date", DateParser.getDateAsString(periodFilter.getParam().getStartDate(), DateParser.YYYYMMDD))
-                .queryParam("end_date", DateParser.getDateAsString(periodFilter.getParam().getEndDate(), DateParser.YYYYMMDD))
+                .queryParam("start_date",
+                        DateParser.getDateAsString(periodFilter.getParam().getStartDate(), DateParser.YYYYMMDD))
+                .queryParam("end_date",
+                        DateParser.getDateAsString(periodFilter.getParam().getEndDate(), DateParser.YYYYMMDD))
                 .build().toUri();
         return downloadData(requestUri);
     }
--- restwebservice/src/main/java/it/michdev/restwebservice/webclient/WebClient.java
+++ restwebservice/src/main/java/it/michdev/restwebservice/webclient/WebClient.java
@@ -6,16 +6,38 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
+/**
+ * La classe astratta <b>WebClient</b> rappresenta un generico REST client con
+ * metodi per effettuare chiamate ad un endpoint definito. Questa classe è
+ * estesa da <code>FxMarketClient</code>.
+ * 
+ * @version 0.8.0
+ * @author Michele Bevilacqua
+ * @see it.michdev.restwebservice.webclient.FxMarketClient
+ */
 public abstract class WebClient {
 
     protected String endpoint;
     private HttpClient httpClient;
 
+    /**
+     * Costruttore per la classe <code>WebClient</code>
+     * 
+     * @param endpoint endpoint a cui effettuare le chiamate HTTP REST.
+     */
     public WebClient(String endpoint) {
         this.endpoint = endpoint;
         this.httpClient = HttpClient.newHttpClient();
     }
-    
+
+    /**
+     * Effettua una chiamata all'endpoint definito tramite <code>URI</code> passata
+     * come argomento e scarica i dati ottenuti come oggetto
+     * <code>HttpResponse</code>.
+     * 
+     * @param requestUri <code>URI</code> dell'endpoint
+     * @return oggetto <code>HttpResponse</code> contenente i dati ottenuti.
+     */
     protected HttpResponse<String> downloadData(URI requestUri) {
         HttpRequest httpRequest = HttpRequest.newBuilder(requestUri).build();
         HttpResponse<String> requestResponse;
@@ -28,5 +50,11 @@ public abstract class WebClient {
         }
     }
 
+    /**
+     * Metodo astratto, implementato nelle sottoclassi di <code>WebClient</code>.
+     * Costruisce un <code>URI</code> ed ottiene i dati della chiamata effettuata.
+     * 
+     * @return oggetto <code>HttpResponse</code> contenente i dati ottenuti.
+     */
     public abstract HttpResponse<String> requestData();
 }
